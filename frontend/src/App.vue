33da3c57b6e912b63e3aca8e6ebc803d62c3900c
Siky <yyymalicious@gmail.com>
2024-01-24 00:59:45 +0800

    又写了一些，好丑的界面啊，还没把流量的数据渲染到界面上，感觉接下来要把每个流量包属于哪个网卡设置一下，然后让左边的有东西可以看

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import ViewNet from './components/ViewNet.vue'
+import ViewNetManage from './components/ViewNetManage.vue';
 </script>
 
 <template>
   <el-row>
-    <el-col :span="6" style="height: 100vh;">
+    <el-col :span="6" style="height: 100vh; margin: 12px;">
+      <div style="margin-bottom: 12px;">
+        <ViewNetManage title="开启请求"/>
+      </div>
       <ViewNet/>
     </el-col>
   </el-row>
---
+++ frontend/src/components/ViewNetManage.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+        <el-button type="primary" @click="dialogVisible = true">
+            {{ title }}
+        </el-button>
+        <el-dialog
+            v-model="dialogVisible"
+            :title="title"
+            width="60%"
+        >
+            <span>This is a message</span>
+            <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                Confirm
+                </el-button>
+            </span>
+            </template>
+        </el-dialog>
+    </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+defineProps(['title'])
+const dialogVisible = ref(false)
+</script>
+
+
+<style scoped>
+</style>
